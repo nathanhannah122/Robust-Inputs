bf3d206760abc7359ce2386a53a371b512abfa73
Nathan Hannah <74415841+nathanhannah122@users.noreply.github.com>
2022-02-14 10:53:08 +0000

    Update login-auth.php
    added additional comments

--- login-auth.php
+++ login-auth.php
@@ -12,7 +12,7 @@
       header('location: index.php');
    } 
 
-   //post form signup form
+   // post data from signup form
    $email      = checkData($_POST['email']);
    $password   = checkData($_POST['password']);
    $HashedPass = hash('sha512', $password.SALT_STRING);
@@ -21,6 +21,7 @@
 
    $nameCheck  = checkName($name);
    $ageCheck   = validateAge($age);
+
    if ($nameCheck == True) {
       $_SESSION['errMessage'] = 'Only Alphabetic chararcters for the name field';
       header('location: index.php');
@@ -31,16 +32,18 @@
       exit();
    }
 
+   // calls function to input to DB  
    processData($name, $email, $age, $HashedPass);
 
-   // sanitizes data
+   // sanitises and returns data
    function checkData($data) {
     $data = trim($data);
     $data = stripslashes($data);
     $data = htmlspecialchars($data);
     return $data;
    }
-
+   
+   // checks name input for alphabetic chars
    function checkName($data){
    if (!preg_match("/^[a-zA-Z ]*$/",$data)) {  
       return True;
@@ -48,6 +51,7 @@
    return False;
    }
    
+   // validates user age, numerical, max and min age
    function validateAge($age) {
     if (preg_match("/^[0-9]+$/", $age)) {
         return true;  
@@ -59,28 +63,30 @@
     return false;
    }
 
+   // inputs values into database
+   // params - name, email, age and SHA 512 hashed password
+   // when successful redirects user to success message
+   function processData($name, $email, $age, $HashedPass) {
+      include 'db-conn.php';
+      try{
+          $sql = "INSERT INTO customer (name, email, age, password) VALUES (:username, :useremail, :userage, :userpassword)";
+          // Prepare statement.
+          $pdo = new PDO($dsn, $username, $passwordDb);
+          $statement = $pdo->prepare($sql);
 
-function processData($name, $email, $age, $HashedPass) {
-   include 'db-conn.php';
-   try{
-       $sql = "INSERT INTO customer (name, email, age, password) VALUES (:username, :useremail, :userage, :userpassword)";
-       // Prepare statement.
-       $pdo = new PDO($dsn, $username, $passwordDb);
-       $statement = $pdo->prepare($sql);
-
-       // Bind values to the parameter.
-       $statement->bindValue(':username', $name);
-       $statement->bindValue(':useremail', $email);
-       $statement->bindValue(':userage', $age);
-       $statement->bindValue(':userpassword', $HashedPass);
-       // Execute the statement and insertvalues.
-       $inserted = $statement->execute();
-       header('location: success.php');
-   // Catch errors
-   } catch (PDOException $e) {
-       echo $e->getMessage();
+          // Bind values to the parameter.
+          $statement->bindValue(':username', $name);
+          $statement->bindValue(':useremail', $email);
+          $statement->bindValue(':userage', $age);
+          $statement->bindValue(':userpassword', $HashedPass);
+          // Execute the statement and insertvalues.
+          $inserted = $statement->execute();
+          header('location: success.php');
+      // Catch errors
+      } catch (PDOException $e) {
+          echo $e->getMessage();
+      }
    }
-}
 
 
 
